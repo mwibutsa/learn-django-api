33e448b24880d6c6d46908ab1e0f399545332a97
mwibutsa <mflohost@gmail.com>
2019-07-22 17:47:44 +0200

    Added permissions and authentication to user profile viewset

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -4,9 +4,14 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from . import serializers
 from rest_framework import status
 from rest_framework import viewsets
+
+from rest_framework.authentication import TokenAuthentication
+
+from . import serializers
+from . import models
+from . import permissions
 
 
 class HelloApiView(APIView):
@@ -104,3 +109,12 @@ class HelloViewSet(viewsets.ViewSet):
         """ Handels removing an object."""
 
         return Response({"http_method": 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """ Handels creating and updating profile """
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
