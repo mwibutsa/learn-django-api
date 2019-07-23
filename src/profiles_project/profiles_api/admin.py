4a3406580209d59d622d9e7e5378c16386cbe515
mwibutsa <mflohost@gmail.com>
2019-07-23 09:40:37 +0200

    added model for storing profile feed item

--- src/profiles_project/profiles_api/admin.py
+++ src/profiles_project/profiles_api/admin.py
@@ -2,7 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile, ProfileFeedItem
 
 admin.site.register(UserProfile)
+admin.site.register(ProfileFeedItem)
 # Register your models here.
